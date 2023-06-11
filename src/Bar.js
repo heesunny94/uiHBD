db0699719ac146c39445c86f79cfddffd2dd392f
heesunny94 <sunnychu1228@kakao.com>
2023-06-12 03:53:17 +0900

    bar name 수정

--- src/Bar.js
+++ src/Bar.js
@@ -5,8 +5,8 @@ function Bar() {
     <nav style={{ display: 'flex', justifyContent: 'center' }}>
       <ul style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
         <li style={{ margin: '0 10px' }}><a href="/">Home</a></li>
-        <li style={{ margin: '0 10px' }}><a href="/about">About</a></li>
-        <li style={{ margin: '0 10px' }}><a href="/services">Services</a></li>
+        <li style={{ margin: '0 10px' }}><a href="/letter">Letter</a></li>
+        <li style={{ margin: '0 10px' }}><a href="/components/cam">Cam</a></li>
         <li style={{ margin: '0 10px' }}><a href="/contact">Contact</a></li>
       </ul>
     </nav>
