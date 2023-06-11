42b5d0c0e53a054d4d67b42b9cc2289bd082adb0
heesunny94 <sunnychu1228@kakao.com>
2023-06-12 03:50:33 +0900

    gif 파일 추가

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Greeting.css';
+import { getByTitle } from '@testing-library/react';
 
 function Greeting() {
   return (
     <section>
-      <h2>Happy Birthday! User Interface</h2>
+      <div class = 'HBD'>
+      <h2>Happy Birthday! User Interface </h2>
+      </div>
+      <img src="/hbd1.gif" alt="HBD" />
       <p>Wishing you a fantastic day filled with joy and laughter.</p>
     </section>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header>
-      <h1>Birthday Website</h1>
+      <h1>Happy Birthday</h1>
     </header>
   );
 }
